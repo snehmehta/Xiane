c4e9818fb7dee43b0ae85a508e66b758ad377d3f
Nimit Patel <nimit2997@gmail.com>
2019-01-28 23:46:15 +0530

    Autoscroll added to V1.1

--- static/main.js
+++ static/main.js
@@ -44,3 +44,14 @@ $('#target').on('submit', function(e){
     // send the message
     submit_message(input_message)
 });
+
+// Get a reference to the div you want to auto-scroll.
+var someElement = document.querySelector('.chat-container');
+// Create an observer and pass it a callback.
+var observer = new MutationObserver(scrollToBottom);
+// Tell it to look for new children that will change the height.
+var config = {childList: true};
+observer.observe(someElement, config);
+function scrollToBottom() {
+    someElement.scrollTop = someElement.scrollHeight;
+  }
