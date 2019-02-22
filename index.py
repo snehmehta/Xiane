2a7cbbe8d67683205c35646c9a3e6edf1a151da7
Sneh Mehta <snehmehta933@gmail.com>
2019-02-22 15:07:23 +0530

    mechanical added

--- index.py
+++ index.py
@@ -15,6 +15,11 @@ with open("everything.txt",'r') as f:
 with open("computer.txt",'r') as f:
     computer_text = f.read()
 
+with open("chemical_text.txt",'r') as f:
+    chemical_text = f.read()
+
+with open("mech_text.txt",'r') as f:
+    mech_text = f.read()
 
 def first_entity_value(entities, entity):
     if entity not in entities:
@@ -31,6 +36,9 @@ def handle_message(response):
     creator = first_entity_value(entities,'personality')
     everything = first_entity_value(entities,'fest_question')
     computer = first_entity_value(entities,'computer')
+    mechanical = first_entity_value(entities,'mechanical')
+    civil = first_entity_value(entities,'civil')
+    chemical = first_entity_value(entities,'chemical')
 
     if creator:
         return ("BrainChild of Sneh Mehta and Beautify by Nimit Patel",'text')
@@ -49,9 +57,31 @@ def handle_message(response):
             return ('web_tricks','iframe')
         elif computer == 'cyber hunt':
             return ('cyber_hunt','iframe')
+        
+    elif mechanical:
+        if mechanical == 'general':
+            return (mech_text, 'text')
+        elif mechanical == 'robo race':
+            return ('robo_race','iframe')
+        elif mechanical == 'lathe war':
+            return ('lathe_war','iframe')
+        elif mechanical == 'cad champs':
+            return ('cad_champs','iframe')
+        elif mechanical == 'mech quiz':
+            return ('mech_quizs','iframe')
 
+    elif chemical:
+        if chemical == 'general':
+            return (chemical_text,'text')
+        elif chemical == 'chem-o-quiz':
+            return ('chem-o-quiz','iframe')
+        elif chemical == 'chem-o-flow':
+            return ('chem-o-flow','iframe')
+        elif chemical == 'chem-o-car':
+            return ('chem-o-car','iframe')
+        
     else:
-        return ("I am under Construction, please try later",'text')
+        return ("Sorry I dont get, what you are saying",'text')
 
 
 @app.route('/')
@@ -89,6 +119,34 @@ def web_tricks():
 @app.route('/relay_coding')
 def relay_coding():
     return render_template('relay_coding.html') 
+
+@app.route('/mech_quiz')
+def mech_quiz():
+    return render_template('mech_quiz.html') 
+
+@app.route('/robo_race')
+def robo_race():
+    return render_template('robo_race.html') 
+
+@app.route('/lathe_war')
+def lathe_war():
+    return render_template('robo_race.html') 
+
+@app.route('/cad_champs')
+def cad_champs():
+    return render_template('cad_champs.html') 
+
+@app.route('/chem-o-car')
+def chem_o_car():
+    return render_template('chem-o-car.html') 
+
+@app.route('/chem-o-flow')
+def chem_o_flow():
+    return render_template('chem-o-flow.html') 
+
+@app.route('/chem-o-quiz')
+def chem_o_quiz():
+    return render_template('chem-o-quiz.html') 
 
 # run Flask app
 if __name__ == "__main__":
