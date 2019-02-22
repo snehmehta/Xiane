7ec93f79e3d04c0dd2e2807b1b65e4cabface4cb
Sneh Mehta <snehmehta933@gmail.com>
2019-02-23 00:12:54 +0530

    all events added

--- index.py
+++ index.py
@@ -9,8 +9,8 @@ access_token = "DXRQXBGEAYGVY5PKLUELZW4BFQWWPWXW"
 
 app = Flask(__name__)
 
-with open("everything.txt",'r') as f:
-    everything_text = f.read()
+# with open("everything.txt",'r') as f:
+#     everything_text = f.read()
 
 with open("computer.txt",'r') as f:
     computer_text = f.read()
@@ -23,6 +23,16 @@ with open("mech_text.txt",'r') as f:
 
 with open("civil_text.txt",'r') as f:
     civil_text = f.read()
+
+with open("electrical_text.txt",'r') as f:
+    electrical_text = f.read()
+
+with open("general_text.txt",'r') as f:
+    general_text = f.read()
+
+with open("gaming_text.txt",'r') as f:
+    gaming_text = f.read()
+
 
 def first_entity_value(entities, entity):
     if entity not in entities:
@@ -42,13 +52,18 @@ def handle_message(response):
     mechanical = first_entity_value(entities,'mechanical')
     civil = first_entity_value(entities,'civil')
     chemical = first_entity_value(entities,'chemical')
+    electrical = first_entity_value(entities,'electrical')
+    gaming = first_entity_value(entities,'gaming')
+    general = first_entity_value(entities,'general')    
+    invalid = first_entity_value(entities,'invalid')    
+
 
     if creator:
         return ("BrainChild of Sneh Mehta and Beautify by Nimit Patel",'text')
     elif greeting:
-        return (everything_text,'text')
+        return ('everything','iframe')
     elif everything:
-        return (everything_text,'text')
+        return ('everything','iframe')
     elif computer:
         if computer == 'general':
             return (computer_text,'text')
@@ -86,14 +101,53 @@ def handle_message(response):
     elif civil:
         if civil == 'general':
             return (civil_text, 'text')
-        elif civil == 'sustainable-planning':
-            return ('sustainable-planning','iframe')
-        elif civil == 'sarjan-setu':
-            return ('sarjan-setu','iframe')
-        elif civil == 'civil-quiz':
-            return ('civil-quiz','iframe')
+        elif civil == 'sustainable planning':
+            return ('sustainable_planning','iframe')
+        elif civil == 'sarjan setu':
+            return ('sarjan_setu','iframe')
+        elif civil == 'civil quiz':
+            return ('civil_quiz','iframe')
         elif civil == 'aakruti':
             return ('aakruti','iframe')
+        
+    elif electrical:
+        if electrical == 'general':
+            return (electrical_text, 'text')
+        elif electrical == 'magnetic labyrinth':
+            return ('magnetic_labyrinth','iframe')
+        elif electrical == 'circuitrix':
+            return ('circuitrix','iframe')
+        elif electrical == 'laser maze':
+            return ('laser_maze','iframe')
+        elif electrical == 'electrical quiz':
+            return ('electrical_quiz','iframe')
+        elif electrical == 'electrical analogy':
+            return ('electrical_analogy','iframe')
+        
+    elif general:
+        if general == 'general':
+            return (general_text, 'text')
+        elif general == 'treasure hunt':
+            return ('treasure_hunt','iframe')
+        elif general == 'mock placement':
+            return ('mock_placement','iframe')
+        elif general == 'paper presentation':
+            return ('paper_presentation','iframe')
+        elif general == 'poster presentation':
+            return ('poster_presentation','iframe')
+
+    elif gaming:
+        if gaming == 'general':
+            return (gaming_text, 'text')
+        elif gaming == 'gully cricket':
+            return ('gully_cricket','iframe')
+        elif gaming == 'cs go':
+            return ('cs_go','iframe')
+        elif gaming == 'pubg':
+            return ('pubg','iframe')
+
+    elif invalid:
+        return ('I can only anwser questions related to TekXianze')
 
     else:
         return ("Sorry I dont get, what you are saying",'text')
@@ -118,6 +172,10 @@ def get_message():
         }
 
         return jsonify(reply)
+
+@app.route('/everything')
+def everything():
+    return render_template('everything.html') 
 
 @app.route('/c_quiz')
 def c_quiz():
@@ -163,21 +221,66 @@ def chem_o_flow():
 def chem_o_quiz():
     return render_template('chem-o-quiz.html') 
 
-@app.route('/sustainable-planning')
+@app.route('/sustainable_planning')
 def sustainable_planning():
-    return render_template('sustainable-planning.html')
+    return render_template('sustainable_planning.html')
 
-@app.route('/sarjan-setu')
+@app.route('/sarjan_setu')
 def sarjan_setu():
-    return render_template('sarjan-setu.html')
+    return render_template('sarjan_setu.html')
 
-@app.route('/civil-quiz')
+@app.route('/civil_quiz')
 def civil_quiz():
-    return render_template('civil-quiz.html') 
+    return render_template('civil_quiz.html') 
 
 @app.route('/aakruti')
 def aakruti():
     return render_template('aakruti.html') 
+
+@app.route('/laser_maze')
+def laser_maze():
+    return render_template('laser_maze.html') 
+
+@app.route('/electrical_analogy')
+def electrical_analogy():
+    return render_template('electrical_analogy.html') 
+
+@app.route('/magnetic_labyrinth')
+def magnetic_labyrinth():
+    return render_template('magnetic_labyrinth.html') 
+
+@app.route('/electrical_quiz')
+def electrical_quiz():
+    return render_template('electrical_quiz.html') 
+
+@app.route('/poster_presentation')
+def poster_presentation():
+    return render_template('poster_presentation.html') 
+
+@app.route('/paper_presentation')
+def paper_presentation():
+    return render_template('paper_presentation.html') 
+
+@app.route('/mock_placement')
+def mock_placement():
+    return render_template('mock_placement.html') 
+
+@app.route('/treasure_hunt')
+def treasure_hunt():
+    return render_template('treasure_hunt.html') 
+
+@app.route('/gully_cricket')
+def gully_cricket():
+    return render_template('gully_cricket.html') 
+
+@app.route('/cs_go')
+def cs_go():
+    return render_template('cs_go.html') 
+
+@app.route('/pubg')
+def pubg():
+    return render_template('pubg.html') 
+
 
 # run Flask app
 if __name__ == "__main__":
